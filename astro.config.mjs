6b90f6225f838a56fa3cf6d5cb6feed8e9c93585
SantiagoXT <134119915+SantiagoXT@users.noreply.github.com>
2024-04-09 19:47:14 -0600

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
+import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), db()],
+  output: "server",
+  adapter: vercel()
+});
